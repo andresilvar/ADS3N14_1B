697c4292e7d2c492aad4e9a99f4bded42ab5474a
Andre da Silva Rosa <andre_silvar@yahoo.com.br>
2014-03-20 16:55:25 -0300

    HelloWolrd Modificado para HelloYou

--- HelloYou/src/Principal.java
+++ HelloYou/src/Principal.java
@@ -5,13 +5,9 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("Hello, You");
 		// TODO Auto-generated method stub
 
-<<<<<<< HEAD
-		System.out.println("Hello, You");
-=======
-		System.out.println("Hello You");
->>>>>>> 5ee81fb4557c783e794406714ef65e0de538f1ef
 	}
 
 }
